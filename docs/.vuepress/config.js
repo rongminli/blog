6d728e172d99d68cdd684df9cdbff3e1dab65d92
rongminli <1055310405@qq.com>
2022-11-02 14:51:07 +0800

    完成第六章 第一节 枚举

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,12 +6,16 @@ module.exports = {
         nav: [
             { text: 'Home', link: '/' }
         ],
-        sidebar:{
+        sidebar: {
             '/rust/': [
                 '6.enums_and_pattern_matching',
                 '8.common_collections'
             ]
         },
-        sidebarDepth: 2
+        sidebarDepth: 2,
+
+    },
+    markdown: {
+        lineNumbers: true
     }
 }
